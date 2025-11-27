9383e26ceef360b7d6cf256d78535b624e4d6f4f
sekyungnetadmin <info@sekyungtex.co.kr>
2025-11-27 15:44:36 +0900

    fix layout children type

--- app/en/layout.tsx
+++ app/en/layout.tsx
@@ -1,8 +1,9 @@
 import "../globals.css";
 import TopNav from "@/components/TopNav";
 import EnFooter from "@/components/EnFooter";
+import { ReactNode } from "react";
 
-export default function EnLayout({ children }) {
+export default function EnLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="font-ko bg-white text-[#20262A]">
--- app/ko/layout.tsx
+++ app/ko/layout.tsx
@@ -1,12 +1,12 @@
 import "../globals.css";
 import KrTopNav from "./KrTopNav";
 import KoFooter from "./KoFooter";
+import { ReactNode } from "react";
 
 export const metadata = {
   title: "세경네트 - 한국어",
 };
-
-export default function KoLayout({ children }) {
+export default function KoLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
       <body className="font-ko">
