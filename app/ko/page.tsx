82a919238216335afa686a66ea6228f9ab3ae50d
sekyungnetadmin <info@sekyungtex.co.kr>
2025-12-04 11:53:30 +0900

    caps

--- app/ko/page.tsx
+++ app/ko/page.tsx
@@ -4,8 +4,8 @@ export const metadata = {
     "세경네트는 골프연습장용 안전망, 야구 배팅 케이지망, 산업용 안전망 등 다양한 스포츠 네트 제품을 제조 및 공급하는 전문 기업입니다.",
 };
 
-import MainKo from "./Mainko";
+import MainKo from "./MainKo";
 
 export default function Page() {
-  return <Mainko />;
+  return <MainKo />;
 }
