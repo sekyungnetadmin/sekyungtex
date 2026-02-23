e5de22690039b00ea7709a0b08b0a6ad57e7f56b
sekyungnetadmin <info@sekyungtex.co.kr>
2026-02-23 12:12:23 +0900

    test

--- app/head.tsx
+++ app/head.tsx
@@ -1,6 +1,7 @@
 export default function Head() {
   return (
     <>
+     <title>SEKYUNGTEX TEST</title>
       <meta
         name="naver-site-verification"
         content="a724d3c7a5089923521507a7f2a7dac0e8171d84"
